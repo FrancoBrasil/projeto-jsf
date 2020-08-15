bdf4d0a13132f32e6f95c57556187a7ac18150a1
FrancoBrasil <fcbs.web@gmail.com>
2020-08-15 10:29:32 -0300

    Ligando atributos com expression language jsf

--- src/main/java/br/com/projetojsf/PessoaBean.java
+++ src/main/java/br/com/projetojsf/PessoaBean.java
@@ -7,6 +7,12 @@ public class PessoaBean {
 
 	private String nome;
 	private String sobrenome;
+	private String nomeCompleto;
+
+	public String mostrarNome() {
+		return nomeCompleto = nome + " " + sobrenome;
+
+	}
 
 	public String getNome() {
 		return nome;
@@ -22,6 +28,14 @@ public class PessoaBean {
 
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
 	}
 
 }
