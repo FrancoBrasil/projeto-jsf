b4e0df88b8f4f4ee649c31f7d550edd18418122f
FrancoBrasil <fcbs.web@gmail.com>
2020-08-18 10:58:37 -0300

    Delete com JPA e JSF

--- src/main/java/br/com/dao/DaoGeneric.java
+++ src/main/java/br/com/dao/DaoGeneric.java
@@ -3,6 +3,7 @@ package br.com.dao;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
+import br.com.entidades.Pessoa;
 import br.com.jpautil.JPAUtil;
 
 public class DaoGeneric<E> {
@@ -13,6 +14,42 @@ public class DaoGeneric<E> {
 		entityTransaction.begin();
 		
 		entityManager.persist(entidade);
+		
+		entityTransaction.commit();
+		entityManager.close();
+	}
+	
+	public void delete(E entidade) {
+		EntityManager entityManager = JPAUtil.getEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		
+		entityManager.remove(entidade);
+		
+		entityTransaction.commit();
+		entityManager.close();
+	}
+	
+	public void delete1(E entidade) {
+		EntityManager entityManager = JPAUtil.getEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		
+		Pessoa p = new Pessoa();
+		entityManager.remove(entityManager.getReference(entidade.getClass(), p.getId()));
+		
+		entityTransaction.commit();
+		entityManager.close();
+	}
+	
+	public void deletePorId(E entidade) {
+		EntityManager entityManager = JPAUtil.getEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		
+		Object id = JPAUtil.getPrimaryKey(entidade);
+		entityManager.createQuery("delete from " + entidade.getClass().getCanonicalName() + " where id = " + id).executeUpdate();
+	
 		
 		entityTransaction.commit();
 		entityManager.close();
