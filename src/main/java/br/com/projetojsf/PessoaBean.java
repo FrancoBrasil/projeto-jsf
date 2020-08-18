72f178d4dbf11403bbffdbcbbdc9627a24815eb3
FrancoBrasil <fcbs.web@gmail.com>
2020-08-18 11:51:23 -0300

    DataTable - Carregando dados com JSF e JPA

--- src/main/java/br/com/projetojsf/PessoaBean.java
+++ src/main/java/br/com/projetojsf/PessoaBean.java
@@ -1,5 +1,9 @@
 package br.com.projetojsf;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
@@ -12,10 +16,17 @@ public class PessoaBean {
 
 	private Pessoa pessoa = new Pessoa();
 	private DaoGeneric<Pessoa> daoGeneric = new DaoGeneric<Pessoa>();
+	private List<Pessoa> pessoas = new ArrayList<Pessoa>();
 
 	public String salvar() {
 		pessoa = daoGeneric.merge(pessoa);
+		carregarPessoas();
 		return "";
+	}
+	
+	@PostConstruct
+	public void carregarPessoas() {
+		pessoas = daoGeneric.getListEntity(Pessoa.class);
 	}
 	
 	public String novo() {
@@ -26,6 +37,7 @@ public class PessoaBean {
 	public String remove() {
 		daoGeneric.deletePorId(pessoa);
 		pessoa = new Pessoa();
+		carregarPessoas();
 		return "";
 	}
 
@@ -43,6 +55,10 @@ public class PessoaBean {
 
 	public void setDaoGeneric(DaoGeneric<Pessoa> daoGeneric) {
 		this.daoGeneric = daoGeneric;
+	}
+	
+	public List<Pessoa> getPessoas() {
+		return pessoas;
 	}
 
 }
